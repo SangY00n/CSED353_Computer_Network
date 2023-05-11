eea0a7c9b4c90f3756ac802e95a6bf8b8b776071
SangY00n <dbs1367@postech.ac.kr>
2023-05-11 15:50:12 -0700

    Implement: recv_frame() in network_interface.cc

--- libsponge/network_interface.cc
+++ libsponge/network_interface.cc
@@ -74,7 +74,86 @@ void NetworkInterface::send_datagram(const InternetDatagram &dgram, const Addres
 
 //! \param[in] frame the incoming Ethernet frame
 optional<InternetDatagram> NetworkInterface::recv_frame(const EthernetFrame &frame) {
-    DUMMY_CODE(frame);
+    const EthernetHeader &frame_header = frame.header();
+
+    // should ignore any frames not destined for the network interface
+    // (dst is either the broadcase addr or the interface's own Ethernet addr)
+    if (!(frame_header.dst == ETHERNET_BROADCAST || frame_header.dst != _ethernet_address)) {
+        return {};
+    }
+
+    // if frame is IPv4,
+    if (frame_header.type == EthernetHeader::TYPE_IPv4) {
+        InternetDatagram dgram;
+        ParseResult parsing_result = dgram.parse(frame.payload()); // parse the payload as an InternetDatagram
+        if (parsing_result == ParseResult::NoError) { // if successful
+            return dgram;
+        } else { // if not successful
+            return {};
+        }
+    }
+    // if frame is ARP,
+    else if (frame_header.type == EthernetHeader::TYPE_ARP) {
+        ARPMessage msg;
+        ParseResult parsing_result = msg.parse(frame.payload()); // parse the payload as an ARPMessage
+        if (parsing_result == ParseResult::NoError) { // if successful
+            // cache the mapping btw sender's IP addr and Ethernet addr
+            _ethernet_addr_cache[msg.sender_ip_address] = make_pair(msg.sender_ethernet_address, 0);
+
+            // if sender's IP addr match with our sent ARP request, update _arp_timer
+            if (_arp_timer.count(msg.sender_ip_address) != 0) {
+                _arp_timer.erase(msg.sender_ip_address);
+            }
+
+            // if the ARP message is the ARP reply corresponding to the ARP request broadcasted before,
+            // send queued IP datagram
+            std::list<std::pair<InternetDatagram, uint32_t>>::iterator it = _queueing_IP_datagrams.begin();
+            while (it != _queueing_IP_datagrams.end()) {
+                if((*it).second == msg.sender_ip_address) {
+                    // EthernetFrame frame_to_send;
+                    // EthernetHeader header_to_send;
+                    // header_to_send.src = _ethernet_address;
+                    // header_to_send.dst = msg.sender_ethernet_address;
+                    // header_to_send.type = EthernetHeader::TYPE_IPv4;
+                    // frame_to_send.header() = header_to_send;
+                    // frame_to_send.payload() = (*it).first.serialize();
+
+                    // _frames_out.push(frame_to_send);
+
+                    send_datagram((*it).first, Address::from_ipv4_numeric((*it).second));
+
+                    _queueing_IP_datagrams.erase(it++);
+                } else {
+                    it++;
+                }
+            }
+
+            // if the ARP message is an ARP request asking for our IP addr, send an appropriate ARP reply
+            if (msg.opcode == ARPMessage::OPCODE_REQUEST && msg.target_ip_address == _ip_address.ipv4_numeric()) {
+                ARPMessage arp_rep;
+                arp_rep.opcode = ARPMessage::OPCODE_REPLY;
+                arp_rep.sender_ethernet_address = _ethernet_address;
+                arp_rep.sender_ip_address = _ip_address.ipv4_numeric();
+                arp_rep.target_ethernet_address = msg.sender_ethernet_address;
+                arp_rep.target_ip_address = msg.sender_ip_address;
+
+
+                EthernetFrame frame_to_send;
+                EthernetHeader frame_header;
+                frame_header.src = _ethernet_address;
+                frame_header.dst = msg.sender_ethernet_address;
+                frame_header.type = EthernetHeader::TYPE_ARP;
+                frame_to_send.header() = frame_header;
+                frame_to_send.payload() = arp_rep.serialize();
+
+                _frames_out.push(frame_to_send);
+            }
+
+        } else {
+            return {};
+        }
+    }
+
     return {};
 }
 
@@ -82,19 +161,19 @@ optional<InternetDatagram> NetworkInterface::recv_frame(const EthernetFrame &fra
 void NetworkInterface::tick(const size_t ms_since_last_tick) { 
     std::list<uint32_t> keys_to_erase;
 
-    for (auto i: _ethernet_addr_cache) {
+    for (auto &i: _ethernet_addr_cache) {
         i.second.second += ms_since_last_tick;
         if (i.second.second >= CACHE_VALID_TIME) {
             keys_to_erase.push_back(i.first);
         }
     }
 
-    for (auto k: keys_to_erase) {
+    for (auto &k: keys_to_erase) {
         std::unordered_map<uint32_t, std::pair<EthernetAddress, size_t>>::iterator it = _ethernet_addr_cache.find(k);
         _ethernet_addr_cache.erase(it);
     }
 
-    for (auto i: _arp_timer) {
+    for (auto &i: _arp_timer) {
         i.second += ms_since_last_tick;
     }
 }
