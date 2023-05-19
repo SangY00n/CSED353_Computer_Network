8a4d676d2bc40cdb283647092218af8d2a9cc1d5
SangY00n <dbs1367@postech.ac.kr>
2023-05-19 13:56:37 -0700

    Implement: route_one_datagram() in router.cc

--- libsponge/router.cc
+++ libsponge/router.cc
@@ -39,8 +39,38 @@ void Router::add_route(const uint32_t route_prefix,
 
 //! \param[in] dgram The datagram to be routed
 void Router::route_one_datagram(InternetDatagram &dgram) {
-    DUMMY_CODE(dgram);
-    // Your code here.
+    // The router decrements the datagram's TTL(time to live).
+    // Only if the TTL is larger than 1, the router decrements it.
+    if (dgram.header().ttl > 1) {
+        dgram.header().ttl--;
+    }
+    // Otherwise, the router should drop the datagram...
+    else {
+        return;
+    }
+
+    // Since the _routing_table is sorted in descending order of prefix_length,
+    // the first match will be the "longest-prefix match".
+    for (auto &r: _routing_table) {
+        const uint32_t bitmask = (r.prefix_length == static_cast<uint8_t>(32)) ? static_cast<uint32_t>(0) : (static_cast<uint32_t>(-1) << (32 - r.prefix_length));
+        const uint32_t dst_addr_prefix = dgram.header().dst & bitmask;
+
+        // If the route r matches with dst addr, send datagram.
+        if(r.route_prefix == dst_addr_prefix) {
+            // If the next_hop is an empty optional, the next_hop for send_datagram() is the datagram's destination address.
+            if(!r.next_hop.has_value()) {
+                interface(r.interface_num).send_datagram(dgram, Address::from_ipv4_numeric(dgram.header().dst));
+            }
+            // Otherwise, the route's next_hop contains the IP address of the next router along the path.
+            else {
+                interface(r.interface_num).send_datagram(dgram, r.next_hop.value());
+            }
+            
+            break;
+        }
+    }
+
+    // If no routes matched, the router drops the datagram...
 }
 
 void Router::route() {
